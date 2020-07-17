8391a60baf38c277fdc703cd1ed94a31ef417d89
jbrisibe10 <43288228+jbrisibe10@users.noreply.github.com>
2020-07-17 14:46:10 +0100

    add first Get request

--- server.js
+++ server.js
@@ -1,4 +1,13 @@
 const express = require("express");
 const app = express();
 
+const mockUserData = [{ name: "Mark" }, { name: "Jill" }];
+app.get("/users", function (req, res) {
+  res.json({
+    success: true,
+    message: "successfully got users. Nice!",
+    users: mockUserData,
+  });
+});
+
 app.listen(4000, () => console.log("app is running on port 4000"));
